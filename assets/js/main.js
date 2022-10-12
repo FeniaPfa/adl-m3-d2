29a4e9cae8000ff94a94dec507de7db1db23ae74
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2022-10-11 22:47:04 -0300

    e2 is ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,25 @@ const borderBtn = document.querySelector("#border-btn");
 borderBtn.addEventListener("click", () =>{
   borderImg.classList.toggle("border");
 })
+
+// Ejercicio 2
+
+const sticker1 = document.querySelector("#e-input1");
+const sticker2 = document.querySelector("#e-input2");
+const sticker3 = document.querySelector("#e-input3");
+
+const checkBtn = document.querySelector("#check-stickers");
+const stickerMsg = document.querySelector("#e2-msg");
+
+
+checkBtn.addEventListener("click", () => {
+  let sumaStickers = parseInt(sticker1.value) + parseInt(sticker2.value) + parseInt(sticker2.value);
+  if(sumaStickers > 10) {
+    stickerMsg.textContent = `Llevas ${sumaStickers} stickers !! (╯°□°）╯︵ ┻━┻`
+  } else{
+    stickerMsg.textContent = `Llevas ${sumaStickers} stickers 😎`
+  }
+
+
+})
+
